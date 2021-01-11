304b355396e8ed537dbae82c74c95edca5659a96
Anychek <anna.shalbuzova@mail.ru>
2021-01-12 01:07:44 +0300

    рабочая форма

--- корзина/search-form.js
+++ корзина/search-form.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   	$(".fa-search").click(function(){
     	$(".container-form, .form-search, .categories, .fa-search").toggleClass("active");
     	$("input[type='text']").focus();
+    	$(".header-nav").toggleClass("none");
   	});
   
 });
